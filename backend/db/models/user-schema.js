03239d0fa9569925938cc95a1eee1e0e0bc4e4c6
YashGaur000 <103846663+YashGaur000@users.noreply.github.com>
2023-09-01 13:02:04 +0530

    first commit

--- backend/db/models/user-schema.js
+++ backend/db/models/user-schema.js
@@ -4,7 +4,7 @@ import mongoose from "../connections.js";
 const schema = mongoose.Schema
 const UsersSchema = new schema({
     'firstname': {type:SchemaTypes.String, required: true},
-    'lastname': {type:SchemaTypes.String, required: true,},
+    'lastname': {type:SchemaTypes.String, required: true},
     'email': {type:SchemaTypes.String, required: true},
     'githubhandle': {type:SchemaTypes.String, required: true},
     'password': {type:SchemaTypes.String, required: true},
